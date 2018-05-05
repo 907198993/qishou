4792e216e8266ce187d2015dfd43217c61440d67
907198993 <907198993@qq.com>
2018-05-05 11:55:02 +0800

    上传身份证界面

---
+++ app/src/main/java/com/qifan/qishou/Activity/BillActivity.java
@@ -0,0 +1,34 @@
+package com.qifan.qishou.Activity;
+
+import android.view.View;
+
+import com.qifan.qishou.R;
+import com.qifan.qishou.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2018/5/5.
+ */
+
+public class BillActivity extends BaseActivity {
+
+    @Override
+    protected int getContentView() {
+        setAppTitle("账单明细");
+        return R.layout.activity_bill;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void onViewClick(View v) {
+
+    }
+}
--- app/src/main/java/com/qifan/qishou/Activity/MyWalletActivity.java
+++ app/src/main/java/com/qifan/qishou/Activity/MyWalletActivity.java
@@ -1,15 +1,25 @@
 package com.qifan.qishou.Activity;
 
+import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.qifan.qishou.R;
 import com.qifan.qishou.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2018/5/4.
  */
 
 public class MyWalletActivity extends BaseActivity {
+
+    @BindView(R.id.history_detail_tv)
+    TextView historyDetailTv;
+
     @Override
     protected int getContentView() {
         setAppTitle("我的钱包");
@@ -26,8 +36,14 @@ public class MyWalletActivity extends BaseActivity {
 
     }
 
-    @Override
+
+    @OnClick({R.id.history_detail_tv})
     protected void onViewClick(View v) {
+    switch (v.getId()){
+        case R.id.history_detail_tv:
+            STActivity(BillActivity.class);
+            break;
+    }
 
     }
 }
